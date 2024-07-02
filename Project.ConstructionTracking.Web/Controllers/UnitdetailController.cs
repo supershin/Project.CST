dc0b20b87c947fb30d84f0efbe7daae2828c8a3f
F-147 <firsty.shabby@gmail.com>
2024-07-02 11:54:53 +0700

    Form Check and layout navbar

--- Project.ConstructionTracking.Web/Controllers/UnitdetailController.cs
+++ Project.ConstructionTracking.Web/Controllers/UnitdetailController.cs
@@ -2,19 +2,18 @@
 using Project.ConstructionTracking.Web.Models;
 using Project.ConstructionTracking.Web.Services;
 
-
 namespace Project.ConstructionTracking.Web.Controllers
 {
-    public class TrackingController : Controller
+    public class UnitdetailController : Controller
     {
         private readonly ITrackingService _TrackingService;
 
-        public TrackingController(ITrackingService trackingService)
+        public UnitdetailController(ITrackingService trackingService)
         {
             _TrackingService = trackingService;
         }
 
-        public IActionResult Index(Guid ID)
+        public IActionResult index(Guid ID)
         {
             TrackingUnitView viewModel = _TrackingService.GetTrackingUnit(ID);
             return View(viewModel);
