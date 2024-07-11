5e904a1008cd4257c1b74e9c27b84f05a71dc74e
F-147 <firsty.shabby@gmail.com>
2024-07-11 17:23:59 +0700

    [COMMIT] - Page PJM_Approve , Unitstatus By Project , Unit

---
+++ Project.ConstructionTracking.Web/Controllers/UnitStatusByUnitController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.ConstructionTracking.Web.Controllers
+{
+    public class UnitStatusByUnitController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Project.ConstructionTracking.Web/Models/PJMApproveController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.ConstructionTracking.Web.Models
+{
+    public class PJMApproveController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
