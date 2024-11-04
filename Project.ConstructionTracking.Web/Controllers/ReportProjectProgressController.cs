749731d3b1b694ad4ea4b05d818419cfbc9c5712
SittikronPoti <firsty.shabby@gmail.com>
2024-11-04 17:14:17 +0700

    [commit] - view Report Project Process

---
+++ Project.ConstructionTracking.Web/Controllers/ReportProjectProgressController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.ConstructionTracking.Web.Controllers
+{
+    public class ReportProjectProgressController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
