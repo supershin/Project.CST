ed49e8b3d8f9b87d40f0185b6154111109321035
F-147 <firsty.shabby@gmail.com>
2024-11-24 19:13:33 +0700

    [commit] Navbar Active

---
+++ Project.ConstructionTracking.Web/Infras/Repositories/WebAPIRestRepo.cs
@@ -0,0 +1,14 @@
+﻿namespace Project.ConstructionTracking.Web.Infras.Repositories
+{
+    public class WebAPIRestRepo
+    {
+        public interface IGRVenderrportalRepo
+        {
+
+        }
+        public class GRVenderrportalRepo : IGRVenderrportalRepo
+        {
+
+        }
+    }
+}
